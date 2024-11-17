4daaabe847cf0aae2f1fe360078d8330615519f8
Md Rakibul Hasan <152282042+rakibulhasan02@users.noreply.github.com>
2024-11-18 02:07:49 +0600

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int number;
+        System.out.print("Enter your number :");
+        number=input.nextInt();
+        System.out.println("The number is : "+number);
+
+    }
+    
+}
